1e96197e2fec9a4b58f08f39d6954c9f29f181e0
Your Name <you@example.com>
2022-01-19 09:25:49 +0530

    Name change

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
             <div className="i-left">
                 <div className="i-left-wrapper">
                     <h2 className="i-intro">Hello, My Name is</h2>
-                    <h1 className="i-name">Srinivasan</h1>
+                    <h1 className="i-name">srinivasan</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
                             <div className="i-title-item">Web Developer</div>
